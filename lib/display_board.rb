e22bb5c6a76fea3ba811af8ad95f35eee3643344
Peter Kreppein <pmkreppein@gmail.com>
2018-10-22 17:34:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_rainbow(color_list)
+  puts "R: #{color_list[0]}, O: #{color_list[1]}, Y: #{color_list[2]}, G: #{color_list[3]}, B: #{color_list[4]}, I: #{color_list[5]}, V: #{color_list[6]}"
+end
